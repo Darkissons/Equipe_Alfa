8880e8bb48941613f515ff564cd55f1a51987a41
Inácio Rodrigues <inaciormgalvao@gmail.com>
2021-09-26 00:39:28 -0300

    first commit

---
+++ app/src/main/java/com/example/equipealfa/ActivityProfessor.java
@@ -0,0 +1,14 @@
+package com.example.equipealfa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityProfessor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor);
+    }
+}
---
+++ app/src/main/java/com/example/equipealfa/DesenharView.java
@@ -0,0 +1,14 @@
+package com.example.equipealfa;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DesenharView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desenhar_view);
+    }
+}
