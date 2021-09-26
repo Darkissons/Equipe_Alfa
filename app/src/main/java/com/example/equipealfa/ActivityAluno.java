757eddaacaec20648c00b00b933a61773eb1f6f1
Inácio Rodrigues <inaciormgalvao@gmail.com>
2021-09-26 00:55:38 -0300

    second commit

---
+++ app/src/main/java/com/example/equipealfa/ActivityAluno.java
@@ -0,0 +1,20 @@
+package com.example.equipealfa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityAluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor);
+    }
+
+    public void onClick_play(View view) {
+
+    }
+}
